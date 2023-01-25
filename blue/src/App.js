ee20a0569e0fedba8e200ccf386a90e3316c38c7
Max Stouten <max.stouten@lunar.be>
2023-01-25 20:15:26 +1100

    added pokemons yeah cool

--- blue/src/App.js
+++ blue/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import pokemons from './pokemon.json'
+import Proptypes from "prop-types"
+
+const pokies = pokemons.results.slice(0, 150)
+
+const PokemonRow = ({pokemon}) => {
+    return (
+        <tr>
+            <td>{pokemon.name}</td>
+            <td>{pokemon.type.join(', ')}</td>
+        </tr>
+    )
+}
+
+PokemonRow.propTypes = {
+    pokemon: Proptypes.object,
+    type: Proptypes.arrayOf(Proptypes.string),
+    name: Proptypes.arrayOf(Proptypes.string),
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1 className='title'>
+                Pokemon seach
+            </h1>
+            <table width="100%">
+                <thead>
+                <tr>
+                    <th>
+                        Name
+                    </th>
+                    <th>
+                        Type
+                    </th>
+                </tr>
+                </thead>
+                <tbody>
+                {pokies.map((pokemon, index) => {
+                    return (
+                        <PokemonRow key={pokemon.name + index} pokemon={pokemon}/>
+                    )
+                })}
+                </tbody>
+            </table>
+        </div>
+    );
 }
 
 export default App;
